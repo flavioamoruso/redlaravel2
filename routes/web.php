61054d68e0f5c28523ef0f5b7da8f318a081a070
FLAVIO AMORUSO <flavioamoruso6.com>
2024-04-28 16:48:33 +0200

    creata rotta

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PublicController;
+use App\Http\Controllers\ProductController;
 
 
 Route::get('/',[PublicController::class,'homepage'])->name('homepage');
+
+Route::get('/products/index', [ProductController::class,'index'])->name('products.index');
