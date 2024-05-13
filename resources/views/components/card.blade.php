39f13aed94e374ed34938ae6191ce6e2acb59368
FLAVIO AMORUSO <flavioamoruso6.com>
2024-05-13 15:09:49 +0200

    index sistemato

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,13 @@
+
+
+<div class="card" style="width: 18rem;">
+
+<img src="{{$product->img}}" class="card-img-top" alt="...">
+<div class="card-body">
+<h5 class="card-title">{{$product->name}}</h5>
+<p class="card-text">{{$product->description}}</p>
+<p class="card-text">{{$product->price}} €</p>
+<a href="#" class="btn">Dettagli</a>
+</div>
+
+</div>
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,20 +1,26 @@
 <x-layout>
 
 <div class="container-fluid mt-3">
-    <div class="row">
+    <div class="row text-center">
         <div class="col-12">
             <h2>Ecco le card</h2>
-        </div>
-        <div class="col-12 col-md-6 mt-5">
-            {{-- @dd($products) --}}
-            @foreach($products as $product)
-            <x-card
-            :product="$product"
-            />
-            @endforeach
         </div>
     </div>
 </div>
 
+
+<div class="container-fluid">
+    <div class="row justify-content-center text-center">
+        @foreach($products as $product)
+        <div class="col-10 col-md-3 my-3 mx-3">
+                {{-- @dd($products) --}}
+                <x-card
+                :product="$product"
+                />
+                
+            </div>
+            @endforeach
+    </div>
+</div>
 
 </x-layout>
